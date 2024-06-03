40caa5dc7189f49ea33b18f2de89c6d2e538034d
Wither <izawananakowo@gmail.com>
2024-06-03 16:04:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
+var clickCount=0;
+
 document.getElementById('my-button').addEventListener('click', function() {
-    let clickCount = 0;
     document.getElementById('my-button').innerText = `Clicked ${++clickCount} times!`;
   });
