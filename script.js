9f8bd4b37f9b3a877a236be42dc7a0e23cc5e29f
Wither <izawananakowo@gmail.com>
2024-06-04 17:32:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ var clickCount=0;
 document.getElementById('my-button').addEventListener('click', function() {
     document.getElementById('my-button').innerText = `Clicked ${++clickCount} times!`;
   });
+
+if (clickCount >= 10) {
+    //jump to another page
+    window.location.href = "https://lauderwimberton.github.io/";
+}
