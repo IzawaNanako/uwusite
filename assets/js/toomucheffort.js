fd382e4ff9fd3f5f6ddc2d1bfb4939e4714f604f
Wither <izawananakowo@gmail.com>
2024-08-21 20:27:42 +0900

    minor update
    added 123 and made use of jQuery

--- assets/js/toomucheffort.js
+++ assets/js/toomucheffort.js
@@ -1,37 +1,53 @@
-document.addEventListener('click', toggleFullscreen);
-document.addEventListener('click', toggleCursor);
+$(document).on('click', toggleFullscreen);
+$(document).on('click', toggleCursor);
+
+var isKeyDown = false;
 
 document.addEventListener('keydown', (e) => {
     e.preventDefault();
-    if (e.key === 'j' || e.key === 'J') {
-        $("#drinking").toggle();
-        if (e.key === 'J') {
-            $("#drinking").toggleClass("rotate");
+    if (!isKeyDown) {
+        if (e.key === 'j' || e.key === 'J') {
+            $(".drinking").toggle();
+            if (e.key === 'J') {
+                $(".drinking").toggleClass("rotate");
+            }
+            isKeyDown = true;
+        }
+        if (e.key === 'End') {
+            $(".onetwothree").toggle();
+            isKeyDown = true;
+        }
+        if (e.key === 'Escape') {
+            $("#loading-container").toggleClass("loading");
+            window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            isKeyDown = true;
+        }
+        if (e.key === ' ') {
+            alert("you cannot jump here sadly")
+            isKeyDown = true;
+        }
+        if (e.shiftKey && e.ctrlKey && e.altKey && e.key === 'ScrollLock') {
+            $("#loading-container").toggleClass("loading");
+            $("#combobreak")[0].play();
+            alert("the ultimate madlad\nwhy would you ever press all these???")
+            setTimeout(function() {
+                window.location = "https://google.com/search?q=grass&udm=2";
+            }, 750);
+            isKeyDown = true;
         }
     }
-    if (e.key === 'Escape') {
-        document.documentElement.classList.toggle('loading');
-        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    }
-    if (e.key === ' ') {
-        alert("you cannot jump here sadly")
-    }
-    if (e.shiftKey && e.ctrlKey && e.altKey && e.key === 'ScrollLock') {
-        document.documentElement.classList.toggle('loading');
-        document.getElementById("combobreak").play();
-        alert("the ultimate madlad\nwhy would you ever press all these???")
-        setTimeout(function() {
-             window.location = "https://google.com/search?q=grass&udm=2";
-        }, 750);
-    }
-    // If you are reading this, you are cheating, fuck you, it's not funny anymore >:(
+    // If you are reading this, you are cheating, fuck you, it's not funny anymore. >:(
 });
 
-document.documentElement.classList.toggle('loading');
+document.addEventListener('keyup', (e) => {
+    isKeyDown = false;
+});
+
+$("#loading-container").toggleClass("loading");
 setTimeout(function() {
-    document.documentElement.classList.toggle('loading');
+  $("#loading-container").toggleClass("loading");
 }, 2000);
 
 function toggleCursor() {
-    document.documentElement.classList.toggle('hideCursor');
+    $(document.documentElement).toggleClass('hideCursor');
 }
