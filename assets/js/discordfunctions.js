db2d50da51c97a7af6f56cbba69a364fb4b8b8cd
Saduwub <143506996+IzawaNanako@users.noreply.github.com>
2025-01-22 22:00:13 +0900

    Update discordfunctions.js

--- assets/js/discordfunctions.js
+++ assets/js/discordfunctions.js
@@ -1,3 +1,4 @@
+// I'm literally the admin of this server and I made this webhook in this shitty server public just cuz it's funny. It's not a leak dumbass.
 let webhookUrl = `https://discord.com/api/webhooks/1241999880428191765/PM7g0ceu1CDF3rui9jcCDIbrAndYX8FjV8nzvMdlw6uXLNdlz7VEJfB9UogokLryhOHl`;
 
 document.addEventListener('keydown', (e) => {
