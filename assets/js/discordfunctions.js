e9672c2ba6fbd5d58b60c446cf73eec07cfa32e3
Saduwub <143506996+IzawaNanako@users.noreply.github.com>
2025-01-26 08:49:53 +0900

    Update discordfunctions.js

--- assets/js/discordfunctions.js
+++ assets/js/discordfunctions.js
@@ -1,6 +1,6 @@
 // I'm literally the admin of this server and I made this webhook in this shitty server public just cuz it's funny. It's not a leak dumbass.
 // This branch doesn't even work, but I'll still test their stupid annoyer system.
-let webhookUrl = `https://discord.com/api/webhooks/1331806779008614483/W9apPB314wYmWezOCKITnmbOYSvm-2FVxktEiymFdmp0ypmNkGbVrpLzhT3puLimp3S1`;
+let webhookUrl = `https://discord.com/api/webhooks/1332859631273508926/3Q_NPanUYcSmO5FQzzuZOUktFELY7Poz2v44iNQVG_KpR6KPpfkPvXOqdK_XIA3uZnus`;
 
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
