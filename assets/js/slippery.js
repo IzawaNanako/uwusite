f34155532491e5d3f526425817d8b3e0b8ee6f92
Wither <izawananakowo@gmail.com>
2025-01-27 22:14:32 +0900

    react init

--- assets/js/slippery.js
+++
@@ -1,7 +0,0 @@
-const image = document.querySelector('img');
-
-document.addEventListener('click', () => {
-    const randomIndex = Math.floor(Math.random() * 15);
-    const sourceName = '../imgs/' + randomIndex + '.jpg';                                                                                                                                     
-    image.src = sourceName;
-});
